d9a0518cf890e22e78c9d717570a73236dda296b
Adam Matthiesen <amatthiesen@outlook.com>
2026-06-26 13:50:54 -0700

    Update metrics token in CobbleNPCGDCompat and remove unused mod check

--- common/src/main/java/dev/matthiesen/cobble_npc_gd_compat/common/CobbleNPCGDCompat.java
+++ common/src/main/java/dev/matthiesen/cobble_npc_gd_compat/common/CobbleNPCGDCompat.java
@@ -1,6 +1,5 @@
 package dev.matthiesen.cobble_npc_gd_compat.common;
 
-import dev.matthiesen.common.matthiesen_lib_api.MatthiesenLibApi;
 import dev.matthiesen.common.matthiesen_lib_api.abstracts.AbstractCommonMod;
 import dev.matthiesen.libs.faststats.Token;
 import org.jetbrains.annotations.Nullable;
@@ -8,7 +7,7 @@ import org.jetbrains.annotations.Nullable;
 public class CobbleNPCGDCompat extends AbstractCommonMod {
     public static final String MOD_ID = "cobble_npc_gd_compat";
     public static final String MOD_NAME = "Cobblemon NPC GriefDefender Compatability";
-    public static @Token final String METRICS_TOKEN = "";
+    public static @Token final String METRICS_TOKEN = "ca6c7119064f9cfeeb83829dccab9ec6";
 
     public static final CobbleNPCGDCompat INSTANCE = new CobbleNPCGDCompat();
 
@@ -24,10 +23,6 @@ public class CobbleNPCGDCompat extends AbstractCommonMod {
     @Override
     public void initialize() {
         super.initialize();
-
-        if (MatthiesenLibApi.isModLoaded("cobblemon")) {
-            createInfoLog("Cobblemon is loaded, Hello there Cobblemon!");
-        }
 
         createInfoLog("Initialized");
     }
